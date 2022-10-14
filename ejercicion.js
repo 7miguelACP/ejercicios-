e7b628c207f9730bbcf4b16464af7edd7fa6e7a8
miguel <miguelcervantes.re@gmail.com>
2022-10-14 15:48:23 -0500

    ejercicios con numeros js

---
+++ ejercicion.js
@@ -0,0 +1,15 @@
+
+
+let altura=parseInt(169);
+let altura2=parseFloat(1.69);
+let peso=parseFloat(70.3)
+let alturam=Math.ceil(1.69);
+let pesok=Math.floor(70.3);
+let maximo_valor=(Number.MAX_VALUE + 1);
+
+console.log(altura);
+console.log(altura2);
+console.log(peso);
+console.log(alturam);
+console.log(pesok);
+console.log(maximo_valor);
